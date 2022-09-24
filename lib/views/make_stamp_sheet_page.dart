bd9b5074ac553ec9daca778c3168c155cd4533fc
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-24 16:12:12 +0900

    viewsディレクトリをコミットしたかったのでとりあえずコミット

---
+++ lib/views/make_stamp_sheet_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+/// スタンプシート作成ページ
+class MakeStampSheetPage extends StatefulWidget {
+  const MakeStampSheetPage({super.key});
+
+  @override
+  State<MakeStampSheetPage> createState() => _MakeStampSheetPageState();
+}
+
+class _MakeStampSheetPageState extends State<MakeStampSheetPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
