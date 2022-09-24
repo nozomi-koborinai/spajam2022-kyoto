1927b306b2653c6384f363f7e440dfa834436711
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2022-09-24 17:27:16 +0900

    Swipe機能のベースを作成

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:liquid_swipe/liquid_swipe.dart';
+import 'package:spajam2022_kyoto/states.dart';
 
 void main() {
   runApp(const MyApp());
@@ -29,40 +31,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
+    return LiquidSwipe(pages: States.instance.pages);
   }
 }
--- lib/states.dart
+++ lib/states.dart
@@ -1,6 +1,57 @@
+import 'package:flutter/material.dart';
+
 ///　状態管理用クラス
 class States {
   /// singleton
   States._();
   static final States instance = States._();
+
+  /// スワイプウィジェットのそれぞれのページ
+  final pages = [
+    Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.red,
+        child: Center(child: Text('1人目')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    ),
+    Scaffold(
+      appBar: AppBar(
+        title: Text(''),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.blue,
+        child: Center(child: Text('2人目')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    ),
+    Scaffold(
+      appBar: AppBar(
+        title: const Text(''),
+      ),
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        color: Colors.yellow,
+        child: Center(child: Text('3人目')),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    ),
+  ];
 }
